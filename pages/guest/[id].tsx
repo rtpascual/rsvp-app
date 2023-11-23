6f9a57edb282e00aaa9956a0b1ed0050ae3b5d44
Roshane Pascual <rotp@amazon.com>
2023-11-22 18:28:41 -0800

    add back navigation for add and edit guest pages

--- pages/guest/[id].tsx
+++ pages/guest/[id].tsx
@@ -1,4 +1,5 @@
 import GuestUpdateForm from "@/ui-components/GuestUpdateForm";
+import { Flex, Button } from "@aws-amplify/ui-react";
 import { useRouter } from "next/router"
 
 
@@ -8,6 +9,16 @@ export default function GuestEditForm() {
     const guestId = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id;
 
     return (
-        <GuestUpdateForm id={guestId} onSuccess={() => router.push('/')} />
+        <Flex
+        direction='column'
+        gap='1rem'
+    >
+        <Flex><Button onClick={() => router.back()}>Back</Button></Flex>
+        <GuestUpdateForm
+            id={guestId}
+            onSuccess={() => router.push('/')}
+            overrides={{ rsvp: { label: 'RSVP' }}}
+        />
+    </Flex>
     )
 }
--- pages/guest/add/index.tsx
+++ pages/guest/add/index.tsx
@@ -1,10 +1,18 @@
 import GuestCreateForm from "@/ui-components/GuestCreateForm";
+import { Button, Flex } from "@aws-amplify/ui-react";
 import { useRouter } from "next/router";
 
 export default function GuestAddForm() {
     const router = useRouter();
 
-    return (<GuestCreateForm
+    return (
+    <Flex
+        direction='column'
+        gap='1rem'
+    >
+        <Flex><Button onClick={() => router.back()}>Back</Button></Flex>
+        <GuestCreateForm
         onSuccess={() => router.push('/')}
-        overrides={{ rsvp: { label: 'RSVP' }}} />)
+        overrides={{ rsvp: { label: 'RSVP' }}} />
+    </Flex>)
 }
